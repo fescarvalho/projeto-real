ea77aa7f53e849df7a8e07c089cda759b49826e5
fescarvalho <fernando_scarvalho@hotmail.com>
2021-12-25 18:36:28 -0300

    Cadastrando usuarios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -14,12 +14,12 @@ function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter history={history}>
+        <Router history={history}>
           <Header />
           <Routes />
           <GlobalStyles />
           <ToastContainer autoClose={3000} className="toast-container" />
-        </BrowserRouter>
+        </Router>
       </PersistGate>
     </Provider>
   );
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { get } from 'lodash';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
 
 export default function Register() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (nome.length < 3 || nome.length > 255) {
+      formErrors = true;
+      toast.error('Nome deve ter enter 3 e 255 caracteres');
+    }
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('E-mail Invalido');
+    }
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha deve ter enter 6 e 50 caracteres');
+    }
+
+    if (formErrors) return;
+
+    try {
+      await axios.post('/users/', {
+        nome,
+        password,
+        email,
+      });
+      toast.success('Voce fez seu cadastro');
+      history.push('/login');
+    } catch (e) {
+      const errros = get(e, 'response.data.errros', []);
+
+      errros.map((error) => toast.error(error));
+    }
+  }
+
   return (
     <Container>
-      <h1>Register</h1>
+      <h1>Crie sua conta</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="nome">
+          Nome:
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Seu Nome"
+          />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Seu Email"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Seu Senha"
+          />
+        </label>
+
+        <button type="submit"> Criar minha conta</button>
+      </Form>
     </Container>
   );
 }
